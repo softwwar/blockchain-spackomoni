7901a67e0fc692f35bc2edd2f5f65f80b94baf18
sohanemon <m.sohanemon@gmail.com>
2023-07-02 20:23:09 +0600

    feat: pause on hover

--- app/marquee-slider.tsx
+++ app/marquee-slider.tsx
@@ -3,7 +3,7 @@ import Marquee from 'react-fast-marquee';
 export default function MarqueeSlider() {
   return (
     <section>
-      <Marquee className='bg-[#1d2330] py-2 text-white'>
+      <Marquee pauseOnHover className='bg-[#1d2330] py-2 text-white'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere dolorum
         quaerat ipsum delectus libero, dolores voluptate voluptates
         voluptatibus, veritatis minus nihil accusantium ex distinctio? Aut nisi
