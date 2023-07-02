59c250ea7bf3f4429407a1293bfb93ce69ea1028
sohanemon <m.sohanemon@gmail.com>
2023-07-02 14:43:15 +0600

    feat: selection

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from './hero';
+import Selection from './selection';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Selection />
     </main>
   );
 }
---
+++ app/selection.tsx
@@ -0,0 +1,9 @@
+export default function Selection() {
+  return (
+    <section>
+      <div className='py-20 box'>
+        <p className='text-4xl text-white'>Make Your Selection Below</p>
+      </div>
+    </section>
+  );
+}
