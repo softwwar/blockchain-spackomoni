ec98b8efeee99e858fec7b43d543f5170fbe94ba
Sohan R. Emon ⚛ <m.sohanemon@gmail.com>
2023-12-08 23:25:18 +0600

    Update footer.tsx

--- app/(layout)/footer.tsx
+++ app/(layout)/footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <section className='flex items-center justify-between py-8 text-white font-mont box'>
-      <p>Copyright © 2022 Blockchain Rectification . All rights reserved.</p>
+      <p>Copyright © 2023 Blockchain Rectification . All rights reserved.</p>
       <div>
         <Link href={'/'}>Privacy Policy</Link> •{' '}
         <Link href={'/'}>Terms of Service</Link>
